907d1007fee1793d22801f334936824fd4ae3afb
RaidasGrisk <30408368+RaidasGrisk@users.noreply.github.com>
2017-09-23 23:36:42 +0300

    Update Policy_gradient_breakout.py

--- Policy_gradient_breakout.py
+++ Policy_gradient_breakout.py
@@ -1,4 +1,4 @@
-# http://cs229.stanford.edu/proj2016/report/tripathi-deep-reinforcement-learning-for-atari-games-report.pdf
+# These are the guys whose code I used to understand Policy Gradient from scratch:
 # https://gist.github.com/karpathy/a4166c7fe253700972fcbc77e4ea32c5
 # https://gist.github.com/greydanus/5036f784eec2036252e1990da21eda18
 import numpy as np
@@ -6,17 +6,54 @@ import gym
 import tensorflow as tf
 import pylab as pl
 
-# functions
-def plot_pixels(observation):
-    pl.imshow(observation.reshape(50,72)) # whole space 80, 120 ; not including bricks 72, 50
+""""
+Basic explanation of Policy Gradient:
 
+In policy gradient, neural net outputs a probability for each available action.
+The idea of this method is to train neural net so that probabilities of actions,
+after which you've gotten reward in your past experiences are increased.
+
+For example, the player is in state 'A' (pixel values in this example)
+and he randomly picked action 3 from [1, 2, 3] set of actions.
+This time he got lucky and this action yielded him a reward of 1! Wohoo!
+We've collected this data and now we'll train the network to increase the
+probability of action 3 whatever we are in a state similar or equal to 'A'.
+
+That's the basic idea. Here's a short overview of the main loop.
+
+First you play the game (what's done after each frame):
+1. Pre-process state (observation) and be ready to feed it to neural net.
+2. Input the observation into neural net and sample an action from its output.
+3. Make that action and observe the results: state, reward, ...
+4. Create 'correct' output value (ylabel) of neural net for this state.
+   This step is a little tricky to understand at first. The 'correct' output is
+   always the one you've sampled from neural net. E.g if action 3 was correct,
+   than correct output is [0, 0, 1] where third column corresponds to action 3.
+4. Save the data: state, the 'correct' output and the reward you've got.
+5. Do this for a few (or one) games to collect enough data for training.
+
+After you've collected enough data, do one iteration of training:
+1. Construct gradient loss vector. This step is the core of gradient decent method.
+   It is simply a vector of discounted reward vector values collected during the game.
+   You can make additional transformations, e.g. normalize or shift this vector to improve results.
+2. Adjust the errors of net's final layer. Before backprop, you multiply the errors of
+   your network final layer with this vector of discounted reward to increase errors
+   before actions that led to reward.
+3. As a result of this, after performing optimization using backprop, the probability of
+   a sequence of actions which led to reward (in a given state) will increase.
+4. Clear all the data that you've just trained with and play the game again to collect new data.
+
+"""
+
+# functions
 def prep_observation(observation, zeros_and_ones):
     obs_2d = observation[:, :, 0] # from RGB to R
     obs_2d_cut = obs_2d[93:193, 8:152] # Specific to Breakout: whole space 33:193, 8:152 ; not including bricks 93:193, 8:152
-    obs_2d_cut_ds = obs_2d_cut[::2, ::2] # downsample by 2: a b c d e f d - to - a c e d
+    obs_2d_cut_ds = obs_2d_cut[::2, ::2] # downsample by 2: a b c d e f d -> to -> a c e d
     if zeros_and_ones: obs_2d_cut_ds[obs_2d_cut_ds != 0] = 1 # convert to 0's and 1's
     obs_1d = np.ndarray.flatten(obs_2d_cut_ds, order='C') # array to list
     return obs_1d
+
 
 def forward_propagation(input, O):
     # specify NN
@@ -24,6 +61,7 @@ def forward_propagation(input, O):
     l1 = tf.nn.relu(tf.matmul(input, O['w1']))
     l2 = tf.nn.softmax(tf.matmul(l1, O['w2']))
     return l2
+
 
 def discount_and_normalize(input, discount_const, normalized, frame_shift):
 
@@ -49,6 +87,11 @@ def discount_and_normalize(input, discount_const, normalized, frame_shift):
 
     return data_disc
 
+
+def plot_pixels(observation): # this one is used to plot how input to nn looks like
+    pl.imshow(observation.reshape(50,72)) # whole space 80, 120 ; not including bricks 50, 72
+
+
 # hyperparameters
 learning_rate = 0.002
 reward_shift = 10 # to account for lagging reward (frames)
@@ -56,8 +99,8 @@ reward_discount = 0.99
 obs_discount = 0.8 # discount last frame to account for velocity of objects (used in running_frame)
 training_batch_size = 5 # number of games to perform one optimization step
 neurons = 32
-input_size = 3600 # hand written number if pixel observations are altered
-actions = [0, 1, 2] # true actions are 1, 2, 3 this is used for index
+input_size = 3600 # hand written number indicating number of pixels fed into nn
+actions = [0, 1, 2] # true actions are 1, 2, 3 this is used for indexing
 session_name = 'LR %.4f, RS %d, RD %.2f, TB %d, N %d' % (learning_rate, reward_shift, reward_discount, training_batch_size, neurons) # filename for saving
 
 # initialize tf placeholders
@@ -71,6 +114,7 @@ gradient_loss = tf.placeholder(shape=[None, 1], dtype=tf.float32)
 O = {'w1': tf.Variable(tf.truncated_normal(shape=[input_size, neurons], mean=0, stddev=1/np.sqrt(input_size), dtype=tf.float32)),
      'w2': tf.Variable(tf.truncated_normal(shape=[neurons, len(actions)], mean=0, stddev=1/np.sqrt(neurons), dtype=tf.float32))}
 
+# Run this to load old weights if resuming training
 # Trained_weighs = np.load('session_name' + '.npy').item()
 # O['w1'] = tf.Variable(Trained_weighs['w1'])
 # O['w2'] = tf.Variable(Trained_weighs['w2'])
@@ -91,13 +135,13 @@ train_step = optimizer.apply_gradients(gradients)
 env = gym.make("Breakout-v0")
 observation = env.reset()
 running_observation = prep_observation(observation, zeros_and_ones=True)
-# plot_pixels(prep_observation(observation)) # see how single frame looks like
+# plot_pixels(prep_observation(observation)) # run this to see how single frame looks like
 
 # initialize tf session
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
-# tf.reset_default_graph()
+# tf.reset_default_graph() # run this to clear all global variables
 
 # playing / learning loop
 while True:
@@ -108,18 +152,18 @@ while True:
 
     # create running frame (input to nn)
     running_observation = modified_observation + running_observation * obs_discount
-    running_observation[running_observation < 0.5] = 0 ; running_observation[running_observation > 1.0] = 1
-    # plot_pixels(running_observation) ; pl.pause(1e-10)
+    running_observation[running_observation < 0.5] = 0 ; running_observation[running_observation > 1.0] = 1 # trim
+    # plot_pixels(running_observation) ; pl.pause(1e-10) # run this to plot running frame each frame. Beware of lag.
 
     # sample a policy from the network (using running frame made from preprocessed observations)
     output = sess.run(yprob, feed_dict = {x: np.stack([running_observation])})[0]
     action = np.random.choice(a=len(actions), p=output)
 
-    # create 'correct' nn output (the correct one is always the one that was sampled)
-    ylabel = np.zeros_like(output) ; ylabel[action] = 1
-
     # step the environment and get new observation
     observation, reward, done, info = env.step(action + 1) # +1 for actions 1, 2, 3 instead of 0, 1, 2
+
+    # create 'correct' nn output (the correct one is always the one that was sampled)
+    ylabel = np.zeros_like(output) ; ylabel[action] = 1
 
     # record game history
     x_batch.append(running_observation)
