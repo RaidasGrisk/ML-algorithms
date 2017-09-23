2e7305fc15f1b4671a401afef869ab000d69f429
RaidasGrisk <30408368+RaidasGrisk@users.noreply.github.com>
2017-09-23 18:39:32 +0300

    Update Logistic_regression.py

--- Logistic_regression.py
+++ Logistic_regression.py
@@ -10,17 +10,20 @@ def h(O, x):
 # Cost (and derivative) function
 def J(O, x, y, l, der):
 
+    y_ = h(O, x)
     m = len(x) # number of data points
-    features = len(x.T) # number of features
 
     if der == 0: # return cost
-        return (-1/m) * np.sum(y * np.log(h(O, x)) + (1-y) * np.log(1 - h(O, x))) + np.sum(l * O[-1] ** 2)
+        return (1/m) * np.sum(-y * np.log(y_) - np.subtract(1, y) * np.log(1 - y_)) \
+               + np.sum(np.square(l * O[1:len(O)]))
 
     if der == 1: # return derivative
-        O_der = np.empty(shape=(features, 1)) # empty array to store derivatives
-        for i in range(0, len(O)):
-            O_der[i] = np.sum((h(O, x) - y) * x[:,i].reshape(m, 1))
-        return(O_der)
+        return (1/m) * np.dot((y_ - y).T, x).T
+
+
+# Accuracy
+def accuracy(y, x, O):
+    return np.sum([y == (h(O, x) > 0.5)]) / len(y)
 
 
 # Main
@@ -30,28 +33,27 @@ def J(O, x, y, l, der):
 # O - weights (rows correspond to each column of x)
 
 # Data
-DataPoints = 100
-Features = 5
-
-x = np.sort(np.random.randint(-2, 2, size=(DataPoints, Features+1)), axis=0)
-x[:,0] = 1
-y = np.sort(np.random.randint(0, 2, size = (DataPoints, 1)), axis=0)
-O = np.random.uniform(-0.1, 0.1, size=(Features+1, 1))
+x = np.load('Data/x_data.npy').astype(float)
+y = np.load('Data/y_data.npy')[:,0].reshape((len(x),1)).astype(float)
+x_train = x[0:40000]
+x_test = x[40000:50000]
+y_train = y[0:40000]
+y_test = y[40000:50000]
+O = np.random.uniform(-0.01, 0.01, size=(len(x.T), 1))
 
 # Gradient decent
-steps = 1000
-alpha = 1e-5
-l = 0.2
+steps = 50
+alpha = 1e-3
+l = 0.1
 train_history = []  # list to save gradient progress
 
 for step in range(steps):
-    O = O * (1 - alpha * l / len(y)) - alpha * J(O, x, y, l, der=1)
-    train_history.append(J(O, x, y, l, der=0))
-    print(step, " :", J(O, x, y, l, der=0))
+
+    O = O * (1 - alpha * l / len(y_train)) - alpha * J(O, x_train, y_train, l, der=1)
+
+    cost = J(O, x_train, y_train, l, der=0)
+    acc_train, acc_test = accuracy(y_train, x_train, O), accuracy(y_test, x_test, O)
+    train_history.append((cost, acc_train, acc_test))
+    print(step, " :", 'cost :', cost, 'train accuracy:', acc_train, 'test accuracy: ', acc_test)
 
 pl.plot(train_history)
-
-# plot y and hypothesis
-pl.plot(y, 'ro')
-pl.plot(h(O, x))
-
