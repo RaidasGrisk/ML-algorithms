c3eca48a86fc3d6751fc41e2b74e322acfd4829c
Raidas Grisk <30408368+RaidasGrisk@users.noreply.github.com>
2017-09-25 20:04:23 +0300

    Update DQN_Breakout.py

--- DQN_Breakout.py
+++ DQN_Breakout.py
@@ -21,15 +21,12 @@ Train neural net:
 
 
 class DQNagent():
-    def __init__(self, s_size, a_size, m_size, gamma, eps, eps_d, lr):
-        self.memory = []        # list:    memory
-        self.history = []       # list:    training history for plotting
+    def __init__(self, s_size, a_size, m_size, lr):
+        self.memory = []        # object:    memory
+        self.history = []       # object:    training history for plotting
         self.m_size = m_size    # parameter: memory size
         self.s_size = s_size    # parameter: state size
         self.a_size = a_size    # parameter: action size
-        self.eps = eps          # parameter: random action probability in the beginning (explore-exploit dilemma)
-        self.eps_d = eps_d      # parameter: decay of probability of random action
-        self.gamma = gamma      # parameter: discount for next reward
         self.lr = lr            # parameter: learning rate
         self.Q_values, self.optimization, self.sess, self.cost, self.y, self.x = self.tf_sess() # tf session
 
@@ -37,8 +34,8 @@ class DQNagent():
         self.memory.append((state, action, reward, next_state, done))
         if len(self.memory) > self.m_size: self.memory = self.memory[-self.m_size:]
 
-    def act(self, state):
-        if np.random.uniform(0,1,1) < self.eps:
+    def act(self, state, epsilon):
+        if np.random.uniform(0,1,1) < epsilon:
             action = np.zeros(self.a_size)
             action[np.random.randint(0,self.a_size,1)] = 1
         else:
@@ -53,41 +50,18 @@ class DQNagent():
             mini_batch = self.memory
         return mini_batch
 
-    def tf_sess(self):
-        x = tf.placeholder(shape=[None, self.s_size], dtype=tf.float32)
-        y = tf.placeholder(shape=[None, self.a_size], dtype=tf.float32)
-        O = {'w1': tf.Variable(tf.truncated_normal(shape=[self.s_size, 128], mean=0, stddev=0.01, dtype=tf.float32)),
-             'w2': tf.Variable( tf.truncated_normal(shape=[128, self.a_size], mean=0, stddev=0.01, dtype=tf.float32))}
-        Q_values = tf.matmul(tf.nn.relu(tf.matmul(x, O['w1'])), O['w2'])
-        cost = tf.reduce_mean(tf.square(y - Q_values))
-        optimization = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(cost)
-        init = tf.global_variables_initializer()
-        sess = tf.Session()
-        sess.run(init)
-        return Q_values, optimization, sess, cost, y, x
+    def train(self, training_batch, discount, return_Q_targets):
 
-    def train(self, training_batch, return_Q_targets):
-
-        # convert appended lists into numpy arrays
+        # Convert list to array and un-group stacked data
         training_batch = np.array(training_batch)
-
-        # Ungroup stacked data
         state, action, reward, next_state, done = training_batch[:,0], training_batch[:,1], training_batch[:,2], training_batch[:,3], training_batch[:,4]
 
-        # Estimate predictions of Q_values from state
-        Q_target = self.sess.run(self.Q_values, feed_dict={self.x: np.stack(state)})
-
-        # Estimate predictions of Q_values from next_state
-        Q_target_next = np.amax(self.sess.run(self.Q_values, feed_dict={self.x: np.stack(next_state)}), axis=1)
-
-        # Swapping 0->1 and 1->0
-        Q_target_not_done = 1 - done  # Swapping values. Originally, if done = True, I need True if not Done.
-
-        # Estimate target values of Q_value
-        Q_target_one = reward + 0.95 * Q_target_next * Q_target_not_done
-
-        # Assign target Q values to appropriate action
-        for i in range(len(Q_target_one)):
+        # Calculate target Q values
+        Q_target = self.sess.run(self.Q_values, feed_dict={self.x: np.stack(state)}) # Estimate predictions of Q_values from state
+        Q_target_next = np.amax(self.sess.run(self.Q_values, feed_dict={self.x: np.stack(next_state)}), axis=1) # Estimate predictions of Q_values from next_state
+        Q_target_not_done = 1 - done  # Swapping 0->1 and 1->0. Originally, if done = True, I need True if not Done.
+        Q_target_one = reward + discount * Q_target_next * Q_target_not_done # Estimate target values of Q_value
+        for i in range(len(Q_target_one)): # Assign target Q values to appropriate action
             Q_target[i, :action[i]] = Q_target_one[i]
 
         # Train
@@ -95,48 +69,86 @@ class DQNagent():
 
         if return_Q_targets: return Q_target
 
+    def tf_sess(self):
+
+        x = tf.placeholder(shape=[None, self.s_size], dtype=tf.float32)
+        y = tf.placeholder(shape=[None, self.a_size], dtype=tf.float32)
+        O = {'w1': tf.Variable(tf.truncated_normal(shape=[self.s_size, 64], mean=0, stddev=1/np.sqrt(self.s_size), dtype=tf.float32)),
+             'w2': tf.Variable(tf.truncated_normal(shape=[64, 32], mean=0, stddev=1/np.sqrt(64), dtype=tf.float32)),
+             'w3': tf.Variable(tf.truncated_normal(shape=[32, self.a_size], mean=0, stddev=1/np.sqrt(32), dtype=tf.float32))}
+
+        l1 = tf.nn.relu(tf.matmul(x, O['w1']))
+        l2 = tf.nn.relu(tf.matmul(l1, O['w2']))
+        Q_values = tf.matmul(l2, O['w3'])
+
+        cost = tf.losses.huber_loss(y, Q_values) # tf.reduce_mean(tf.square(y - Q_values))
+        optimization = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(cost)
+        init = tf.global_variables_initializer()
+        sess = tf.Session()
+        sess.run(init)
+
+        return Q_values, optimization, sess, cost, y, x
+
+# Hyperparameters
+learning_rate = 0.0001
+discount = 0.999
+epsilon_max = 1
+epsilon_min = 0.1 # minimum value
+epsilon_d = 0.0003 # 1 - epsilon_d * games_played
+memory_size = 16192 * 2
+mini_batch_size = 512
+train_freq = 4
+
+# Initialize game environment, agent and else
 tf.reset_default_graph()
 env = gym.make('Breakout-ram-v0')
-agent = DQNagent(s_size=len(env.reset()), a_size=len(env.unwrapped.get_action_meanings()), m_size=16192, gamma=0.99, eps=1, eps_d=0.99955, lr=0.0002)
+agent = DQNagent(s_size=len(env.reset()), a_size=len(env.unwrapped.get_action_meanings()), m_size=memory_size, lr=learning_rate)
 pl.ioff() # to turn off plotting in IDE (save it to a file instead) pl.ion() to reverse
 
-state = env.reset()
+# Reset variables
+state = env.reset() / 255
+epsilon = 1
+frame = 0
 score = 0
-game = 0
 running_score = 0
+games_played = 0
+memory_is_full = False
 
+# Main loop
 while True:
 
-    # Chose and performe an action, save experience
-    action = np.argmax(agent.act(state))
+    # Chose and perform an action, save experience
+    action = np.argmax(agent.act(state, epsilon))
     next_state, reward, done, _ = env.step(action)
-    agent.remember(state, action, reward, next_state, done)
+    agent.remember(state, action, reward, next_state / 255, done)
+
+    # Update other variables
+    state = next_state / 255
+    score += reward
+    frame += 1
+    memory_is_full = (len(agent.memory) == memory_size)
 
     # Sample training batch and train
-    training_batch = agent.memory_batch(256)
-    agent.train(training_batch, return_Q_targets=False)
-
-    # Other
-    state = next_state
-    score += reward
+    if memory_is_full and frame % train_freq == 0:
+        training_batch = agent.memory_batch(mini_batch_size)
+        agent.train(training_batch, discount, return_Q_targets=False)
 
     if done:
 
-        # Reset and etc.
+        # Reset, update and etc.
         env.reset()
-        agent.eps *= agent.eps_d
+        if memory_is_full: epsilon = epsilon_max - epsilon_d * games_played if epsilon > epsilon_min else epsilon_min
         running_score = running_score * 0.95 + 0.05 * score
         agent.history.append(running_score)
 
         # Print and save
-        print('Game: %d, Reward: %.2f, Eps: %.2f, Memory: %d' % (game, running_score, agent.eps, len(agent.memory)))
+        print('Game: %d, R: %.2f, Running R %.2f, Eps: %.2f, Memory: %d' % (games_played, score, running_score, epsilon, len(agent.memory)))
         pl.plot(agent.history)
         pl.savefig('DGQ Breakout test.png')
 
-        # Other
+        # Update other variables
+        frame = 0
         score = 0
-        game += 1
+        if memory_is_full: games_played += 1
 
-# outside loop for epsilon, when performing an action instead of inside
-# epsilon decay: epsilon = 1 - games * X
-# if memory is not full, do not train
+
